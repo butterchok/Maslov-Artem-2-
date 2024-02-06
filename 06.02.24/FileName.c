f35c9f404250d076f46ac5e0b4557de2490ab181
butterchok <159155540+butterchok@users.noreply.github.com>
2024-02-06 20:38:03 +0500

    Update FileName.c

--- 06.02.24/FileName.c
+++ 06.02.24/FileName.c
@@ -1,28 +1,64 @@
 #include <stdio.h>
-#include "func.h"
+#include <stdlib.h>
 
-#define rows 3
-#define column 4
+#define MAX_ROWS 3
+#define MAX_COLUMNS 4
 
-//реализовать двумерный массив чисел с не более чем 2 - умя выделениями памяти
+typedef struct {
+    int rows;
+    int columns;
+    int* data;
+} DArray;
+
+DArray createDArray(int rows, int columns) {
+    DArray arr;
+    arr.rows = rows;
+    arr.columns = columns;
+    arr.data = (int*)malloc(rows * columns * sizeof(int));
+
+    if (arr.data == NULL) {
+        printf("ERROR: memory alloc failed\n");
+        exit(1);
+    }
+
+    return arr;
+}
+
+void setElement(DArray* arr, int row, int column, int val) {
+    if (row >= 0 && row < arr->rows && column >= 0 && column < arr->columns) {
+        *(arr->data + (row * arr->columns) + column) = val;
+    }
+    else {
+        printf("ERROR: Out of range\n");
+        exit(1);
+    }
+}
+
+int getElement(DArray* arr, int row, int column) {
+    return *(arr->data + (row * arr->columns) + column);
+}
+
+void freeDArray(DArray* arr) {
+    free(arr->data);
+}
 
 int main() {
-    DArray arr = createDArray(rows, column);
+    DArray arr = createDArray(MAX_ROWS, MAX_COLUMNS);
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < column; j++) {
-            setElement(arr, i, j, i * column + j);
+    for (int i = 0; i < MAX_ROWS; i++) {
+        for (int j = 0; j < MAX_COLUMNS; j++) {
+            setElement(&arr, i, j, i * MAX_COLUMNS + j);
         }
     }
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < column; j++) {
-            printf("%d ", getElement(arr, i, j));
+    for (int i = 0; i < MAX_ROWS; i++) {
+        for (int j = 0; j < MAX_COLUMNS; j++) {
+            printf("%d ", getElement(&arr, i, j));
         }
         printf("\n");
     }
 
-    freeDArray(arr);
+    freeDArray(&arr);
 
     return 0;
 }
